205633f0ad930f1932a7cd42158a1bdabfea7e37
Sarah D Herren <herren.sarah@icloud.com>
2021-05-31 00:16:50 -0500

    fixed missing depency use effect character details

--- src/components/CharacterDetails.js
+++ src/components/CharacterDetails.js
@@ -23,7 +23,7 @@ const CharacterDetails = ({characterUrl, closeStats}) => {
                 setStats(res.data)
                 console.log(res)})
             .catch(err => console.log(err))
-            }, [])
+            },[secureCharUrl])
     
                 
   return(              
